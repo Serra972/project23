59ad283454ce92311359256ffc0e9b708e77e6f7
serra <62421881+Serra972@users.noreply.github.com>
2020-05-05 10:08:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup(){
+createCanvas (800,600);
+
+ rect1 =  createSprite(400, 490, 200, 200);
+
+ rect2 = createSprite(253,440,90,300);
+
+ rect3 = createSprite(549,440,90,300);
+
+ rect4 = createSprite(290,265,30,40);
+
+ rect5 = createSprite(255,265,30,40);
+ 
+ rect6 = createSprite(220,265,30,40);
+
+ rect7 = createSprite(550,265,30,40);
+
+ rect8 = createSprite(515,265,30,40);
+
+ rect9 = createSprite(585,265,30,40);
+
+ rect10 = createSprite(400,355,5,70);
+ 
+ rect11 = createSprite(420,335,40,30);
+}
+
+function draw() {
+  background("purple");  
+  stroke("20");
+  
+
+  drawSprites();
+}
